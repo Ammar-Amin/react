2ec6e097467b92a39abc5715f952f45b386fb5ad
Ammar-Amin <ammaramin5253@gmail.com>
2024-04-24 19:44:26 +0530

    sample react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1
+        className="text-center text-red-500 text-8xl bg-slate-200"
+      >
+        React
+        <i class="ri-thumb-up-fill"></i>
+      </h1>
+    </>
+  );
+}
+
+export default App;
